142d5dfeced745dd631f67c6c0f63ddd2baa50ed
Dania <47312326+makires@users.noreply.github.com>
2020-11-12 00:15:11 +0700

    Initial Commit

---
+++ ClimbGesh&CO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClimbGesh&CO
+//
+//  Created by Tatiana Ilvutikova on 12.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
